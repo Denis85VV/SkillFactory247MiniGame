9f89e2be2493672d29e442acc15766670ea22ecc
Pokemon Library <https://github.com/Denis85VV/PokemonLibrary.git>
2021-02-01 19:00:57 +0300

    add thirdPanAction

--- MiniGame/CustomView.swift
+++ MiniGame/CustomView.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+import UIKit
+
+@IBDesignable class CustomView: UIView {
+  
+  var workingView: UIView!
+  var xibName: String = "CustomView"
+  
+  let darkBlue = UIColor(red: 0, green: 0, blue: 1, alpha: 1)
+  let lightBlue = UIColor(red: 0.1, green: 0.5, blue: 1, alpha: 1)
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setCustomView()
+    backgroundColor = .clear
+  }
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    setCustomView()
+    backgroundColor = .clear
+  }
+  
+  func getFromXib() -> UIView {
+    let bundle = Bundle(for: type(of: self))
+    let xib = UINib(nibName: xibName, bundle: bundle)
+    let view = xib.instantiate(withOwner: self, options: nil).first as! UIView
+    return view
+  }
+  
+  func setCustomView() {
+    workingView = getFromXib()
+    workingView.frame = bounds
+    workingView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    workingView.layer.cornerRadius = workingView.frame.height / 2
+    workingView.backgroundColor = .some(lightBlue)
+    
+    addSubview(workingView)
+  }
+  
+  @IBInspectable var changeView: Bool = false {
+    didSet {
+      if changeView {
+        workingView.backgroundColor = .some(darkBlue)
+        workingView.frame = CGRect(x: 0, y: 0, width: self.frame.height * 1.4, height: self.frame.height * 1.4)
+        workingView.layer.cornerRadius = workingView.frame.height / 2
+        backgroundColor = .clear
+      }
+    }
+  }
+  
+}
--- MiniGame/ViewController.swift
+++ MiniGame/ViewController.swift
@@ -9,11 +9,80 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var firstView: CustomView!
+  @IBOutlet weak var secondView: CustomView!
+  @IBOutlet weak var thirdView: CustomView!
+  @IBOutlet weak var fourthView: CustomView!
+  @IBOutlet weak var fifthView: CustomView!
+  @IBOutlet weak var sixthView: CustomView!
+  @IBOutlet weak var seventhView: CustomView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
 
-
+  @IBAction func panAction(_ gesture: UIPanGestureRecognizer) {
+    let gestureTranslation = gesture.translation(in: view)
+    guard let gestureView = gesture.view else {
+      return
+    }
+    gestureView.center = CGPoint(
+      x: gestureView.center.x + gestureTranslation.x,
+      y: gestureView.center.y + gestureTranslation.y
+    )
+    gesture.setTranslation(.zero, in: view)
+    guard gesture.state == .ended else {
+      return
+    }
+    moveViewViaPanAction(firtsViews: firstView, secondViews: secondView)
+  }
+  
+  @IBAction func secondPanAction(_ gesture: UIPanGestureRecognizer) {
+    let gestureTranslation = gesture.translation(in: view)
+    guard let gestureView = gesture.view else {
+      return
+    }
+    gestureView.center = CGPoint(
+      x: gestureView.center.x + gestureTranslation.x,
+      y: gestureView.center.y + gestureTranslation.y
+    )
+    gesture.setTranslation(.zero, in: view)
+    guard gesture.state == .ended else {
+      return
+    }
+    moveViewViaPanAction(firtsViews: secondView, secondViews: thirdView)
+  }
+  
+  @IBAction func thirdPanAction(_ gesture: UIPanGestureRecognizer) {
+    
+    let gestureTranslation = gesture.translation(in: view)
+    guard let gestureView = gesture.view else {
+      return
+    }
+    gestureView.center = CGPoint(
+      x: gestureView.center.x + gestureTranslation.x,
+      y: gestureView.center.y + gestureTranslation.y
+    )
+    gesture.setTranslation(.zero, in: view)
+    guard gesture.state == .ended else {
+      return
+    }
+    moveViewViaPanAction(firtsViews: thirdView, secondViews: fourthView)
+    
+  }
+  
+  func moveViewViaPanAction(firtsViews: CustomView!, secondViews: CustomView!) {
+    let firstViewFrame = firtsViews.frame
+    let secondViewFrame = secondViews.frame
+    
+    for value in Int(secondViews.frame.minY)...Int(secondViews.frame.maxY) {
+      if Int(firstViewFrame.origin.y) == value {
+        secondViews.changeView = true
+        firtsViews.isHidden = true
+        print("View changed")
+      }
+    }
+    print("View panned")
+  }
+  
 }
-
